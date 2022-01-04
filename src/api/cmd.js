32207f5edf34f0ab65c9260cdf046db3e64eb2d0
AiJiangnan <jnai@gaojihealth.com>
2022-01-04 16:21:51 +0800

    优化CMD返回流

--- src/api/cmd.js
+++ src/api/cmd.js
@@ -3,13 +3,11 @@ import sse from './sse'
 
 const cmd = {
     exec: (cmd) => axios.get('/cmd', { params: { cmd } }),
-    execStream: (cmd) => {
-        return new Promise((resolve, reject) => {
-            if (!cmd) {
-                reject(cmd);
-            }
-            sse('/cmd/stream?cmd=' + cmd, resolve)
-        })
+    execStream: (cmd, cb) => {
+        if (!cmd) {
+            return;
+        }
+        sse('/cmd/stream?cmd=' + cmd, cb)
     },
 }
 
